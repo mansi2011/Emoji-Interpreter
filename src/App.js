1e77f096ef5ecc6fd71a7fdddd750a9522cc3e05
Mansi Srivastava <52784832+mansi2011@users.noreply.github.com>
2021-05-30 13:11:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "😊": "Similing",
+  "😳": "Disbelief",
+  "😔": "Sad",
+  "🔥": "Lit",
+  "❤️": "Love Heart",
+  "😑": "Expressionless"
+};
+
+export default function App() {
+  var [meaning, setEmojiInput] = useState("");
+
+  var emojiList = Object.keys(emojiDictionary);
+
+  function inputEmojiHandler(e) {
+    var emojiInput = e.target.value;
+
+    var meaning = emojiDictionary[emojiInput];
+
+    if (meaning === undefined) {
+      meaning = "Sorry could not recognize!";
+    }
+
+    setEmojiInput(meaning);
+  }
+
+  function clickToGetemojiMeaning(item) {
+    var meaning = emojiDictionary[item];
+    setEmojiInput(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1>Emoji Interpreter</h1>
+      <input className="input" onChange={inputEmojiHandler} />
+      <br />
+      <h2>{meaning}</h2>
+      <h3>Emojis We Know! </h3>
+      {emojiList.map((item) => {
+        return (
+          <span className="emojis" onClick={() => clickToGetemojiMeaning(item)}>
+            {item}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
